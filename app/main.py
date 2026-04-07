284c20159df0f4acc9966832bf699c8306be18f6
Iondan587 <danivan022008@gmail.com>
2026-04-07 19:22:33 +0300

    solution6

--- app/main.py
+++ app/main.py
@@ -1,30 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     storage = {}
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        # Формуємо стабільний ключ із позиційних та іменованих аргументів
-        # Сортування kwargs.items() гарантує, що порядок аргументів не змінить ключ
-        key = (args, tuple(sorted(kwargs.items())))
+        # Формуємо стабільний ключ
+        key = (
+            args, tuple(sorted(kwargs.items()))
+        )
 
         if key in storage:
             print("Getting from cache")
             return storage[key]
         else:
             print("Calculating new result")
-            # Викликаємо функцію з усіма типами аргументів
             result = func(*args, **kwargs)
             storage[key] = result
             return result
 
     return wrapper
-
-
-
-
-
-
-
-
